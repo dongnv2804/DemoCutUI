35f1c7fac8d858a246a5611133693a0934f402b2
dongnv2804 <dongnv2804@gmail.com>
2020-09-18 17:54:24 +0700

    update api

--- src/api/api.js
+++ src/api/api.js
@@ -2,9 +2,7 @@ import axios from "axios";
 
 export const fetchDatafromApi = async () => {
   try {
-    const response = await axios("./db.json");
-    const data = response.data;
-    return data;
+    return (await axios("./db.json")).data;
   } catch (error) {
     throw new error();
   }
